5cedae2cd9c277dde6780a02f56266ccdf82cb4e
TylerFixesTech <tin15009@byui.edu>
2026-05-27 14:44:29 -0600

    article correction

--- ponderW05/movie.js
+++ ponderW05/movie.js
@@ -48,20 +48,21 @@ const movies = [
 
 
         // Code for going through each movie and creating content for that movie
-        
+        const article = document.createElement("article");
+        article.className="movie";
     
           
 
-let html = `<article class="movie">
+let html = `
       <h2>${movie.title}</h2>
       <img src=${movie.imgSrc} alt=${movie.imgAlt}>
       <p><strong>Release Date:</strong> ${movie.date}</p>
       <p><strong>Recommended Age:</strong> ${movie.ages}</p>
       <p><strong>Genre:</strong> ${movie.genre}</p>
       <p><strong>Rating:</strong> <span aria-label="${movie.stars.length} out of 5 stars" role="img">${movie.stars}</span></p>
-      <p id='desc'>${movie.description}</p>
-    </article>`;
+      <p id='desc'>${movie.description}</p>`;
 
-movieList.innerHTML += html;
+article.innerHTML = html;
+movieList.appendChild(article);
 
 });
